aaa3d7ca9315ac391a019de0b77a57b96e3c0127
callumks <callumksx@gmail.com>
2025-06-26 09:34:18 -0400

    Fix modal styling: add box-sizing and improve form reset

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,8 @@
-console.log("✅ Script is connected! Hello from index.js");
-
 const editProfileButton = document.querySelector(".profile__edit-btn");
-console.log("Edit Profile button:", editProfileButton);
 
 const modal = document.querySelector("#edit-profile-modal");
-console.log("Modal:", modal);
 
 const closeButton = modal.querySelector(".modal__close-button");
-console.log("Close button:", closeButton);
 
 const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
@@ -16,23 +11,16 @@ const nameInput = document.querySelector("#profile-name-input");
 const descriptionInput = document.querySelector("#profile-description-input");
 
 const profileFormElement = modal.querySelector(".modal__form");
-console.log("Profile form:", profileFormElement);
 
 const addButton = document.querySelector(".profile__add-btn");
-console.log("New Post button:", addButton);
 
 const newPostModal = document.querySelector("#new-post-modal");
-console.log("New Post modal:", newPostModal);
 
 const newPostCloseButton = newPostModal.querySelector(".modal__close-button");
-console.log("New Post close button:", newPostCloseButton);
 
 const addCardFormElement = newPostModal.querySelector(".modal__form");
 const linkInput = newPostModal.querySelector("#card-image-input");
 const captionInput = newPostModal.querySelector("#card-caption-input");
-console.log("Add card form:", addCardFormElement);
-console.log("Link input:", linkInput);
-console.log("Caption input:", captionInput);
 
 function openModal(modal) {
   modal.classList.add("modal_is-opened");
@@ -61,11 +49,7 @@ function handleProfileFormSubmit(evt) {
 function handleAddCardSubmit(evt) {
   evt.preventDefault();
 
-  console.log("Image link:", linkInput.value);
-  console.log("Caption:", captionInput.value);
-
-  linkInput.value = "";
-  captionInput.value = "";
+  addCardFormElement.reset();
 
   closeModal(newPostModal);
 }
